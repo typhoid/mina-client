815d93dcc9f21217bdb49b0968f9c87ba46e95c2
dinhngoctan.cntt@gmail.com <dinhngoctan.cntt@gmail.com@efd37283-3cfa-b2a8-43a0-3df5d0640c4e>
2012-07-04 13:34:46 +0000

    - add db

---
+++ trunk/src/com/server/MinaServer.java
@@ -0,0 +1,72 @@
+package com.server;
+
+/**
+ * @author Ngoc Tan
+ */
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import com.server.db.DatabaseManager;
+import com.server.handler.MinaServerHandler;
+
+public class MinaServer
+{
+	private static final int PORT = 1234;
+
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("Starting server..");
+
+		//testDB();
+		
+		IoAcceptor acceptor = new NioSocketAcceptor();
+
+		System.out.println("Add Logging Filter..");
+		acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+
+		System.out.println("Add Protocol Codec Filter..");
+		acceptor.getFilterChain().addLast(
+				"codec",
+				new ProtocolCodecFilter(new TextLineCodecFactory(
+						Charset.forName("UTF-8"))));
+
+		System.out.println("Add handler..");
+		acceptor.setHandler(new MinaServerHandler());
+
+		System.out.println("Add config..");
+		acceptor.getSessionConfig().setReadBufferSize(2048);
+		acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+
+		System.out.println("Listening on port " + PORT);
+		acceptor.bind(new InetSocketAddress(PORT));
+	}
+	
+	
+	@SuppressWarnings("unused")
+	private static void testDB()
+	{
+
+		DatabaseManager.open();
+
+		DatabaseManager.createTable("animal",
+				"id INT UNSIGNED NOT NULL AUTO_INCREMENT,"
+						+ "PRIMARY KEY (id),"
+						+ "name CHAR(40), category CHAR(40)");
+
+		DatabaseManager.insert("animal", "name, category",
+				"('snake', 'reptile')," + "('frog', 'amphibian'),"
+						+ "('tuna', 'fish')," + "('racoon', 'mammal')");
+
+		DatabaseManager.close();
+
+	}
+}
---
+++ trunk/src/com/server/db/DatabaseManager.java
@@ -0,0 +1,131 @@
+package com.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.server.Config;
+
+public class DatabaseManager
+{
+	private static Connection _connection = null;
+
+	public static void open()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			_connection = DriverManager.getConnection("jdbc:mysql://"
+					+ Config.DB_SERVER, Config.DB_USERNAME, Config.DB_PASSWORD);
+			System.out.println("Database connection established");
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			System.err.println("Mysql driver not found!");
+		} catch (InstantiationException e)
+		{
+			e.printStackTrace();
+			System.err.println("Mysql driver not found!");
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+			System.err.println("Cannot connect to database server");
+			_connection = null;
+		} catch (IllegalAccessException e)
+		{
+			e.printStackTrace();
+			System.err.println("Mysql driver not found!");
+		}
+	}
+
+	public static void close()
+	{
+		if (_connection != null)
+		{
+			try
+			{
+				_connection.close();
+				System.out.println("Database connection terminated");
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+				System.out.println("Error when Database connection terminating");
+			}
+		}
+	}
+
+	/**
+	 * create table
+	 * 
+	 * @param tableName
+	 * @param columnSet
+	 *            Example: createTable("ABC",
+	 *            "id INT UNSIGNED NOT NULL AUTO_INCREMENT," +
+	 *            "PRIMARY KEY (id)," + "name CHAR(40), category CHAR(40)");
+	 */
+	public static boolean createTable(String tableName, String columnSet)
+	{
+		try
+		{
+			Statement s = _connection.createStatement();
+			Statement s1 = _connection.createStatement();
+
+			// create new tables
+			// String searchTable = "SELECT table_name "
+			// + " FROM information_schema.tables "
+			// + "WHERE table_schema = '" + tableName + "'";
+			//
+			// ResultSet resultSet = s.executeQuery(searchTable);
+			//
+			// if (!resultSet.next())
+
+			s.executeUpdate("DROP TABLE IF EXISTS " + tableName);
+			s.close();
+
+			String sql = "CREATE TABLE " + tableName + "(" + columnSet + ")";
+			s1.execute(sql);
+			s1.close();
+
+		} catch (SQLException exception)
+		{
+			exception.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param tableName
+	 * @param columnNameSet
+	 * @param columnValueSet
+	 * 
+	 *            Example: insert(animal, "name, category",
+	 *            "('snake', 'reptile'),('frog', 'amphibian'),('tuna', 'fish'),('racoon', 'mammal')"
+	 *            );
+	 */
+
+	public static boolean insert(String tableName, String columnNameSet,
+			String columnValueSet)
+	{
+		try
+		{
+			Statement s = _connection.createStatement();
+			int count;
+
+			count = s.executeUpdate("INSERT INTO " + tableName + " ("
+					+ columnNameSet + ")" + " VALUES" + columnValueSet);
+
+			s.close();
+			System.out.println(count + " rows were inserted");
+			
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+}
+
