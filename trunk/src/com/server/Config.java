c52dbf553a65a86dae19d9648814a4a0d279a0a5
dinhngoctan.cntt@gmail.com <dinhngoctan.cntt@gmail.com@efd37283-3cfa-b2a8-43a0-3df5d0640c4e>
2012-07-11 12:05:47 +0000

    - del

--- trunk/src/com/server/Config.java
+++
@@ -1,8 +0,0 @@
-package com.server;
-
-public final class Config
-{
-	public static final String DB_SERVER = "localhost:3306/vending1";
-	public static final String DB_USERNAME = "root";
-	public static final String DB_PASSWORD = "";
-}
