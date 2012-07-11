63d5f42bc50652a6d7655777d46711e464ab1d02
dinhngoctan.cntt <dinhngoctan.cntt@gmail.com>
2012-07-11 12:06:51 +0000

    - modified config

---
+++ src/com/server/CONFIG.java
@@ -0,0 +1,8 @@
+package com.server;
+
+public final class CONFIG
+{
+	public static final String DB_SERVER = "localhost:3306/vending1";
+	public static final String DB_USERNAME = "root";
+	public static final String DB_PASSWORD = "";
+}
